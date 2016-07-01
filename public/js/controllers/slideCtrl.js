d96b0c43a4743d6c2cbc3785912c3d9ba02b6b2e
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-07-01 05:46:40 +0200

    audio template dynamic

--- public/js/controllers/slideCtrl.js
+++ public/js/controllers/slideCtrl.js
@@ -5,32 +5,31 @@ angular.module('MyApp')
   angular.element(document).ready(function () {
     var container = angular.element(document.querySelector("[ui-view='partial']"));
     var hello = NavBar.createNavBar(container);
+
+    if($scope.data.type === "video")
+    {
+        Video.initEvent($scope.data.nextChapter,$scope.data.nextSlide);
+    }
+    else if($scope.data.type === "audios")
+    {
+        $scope.wave = [];
+        for(var i=0, l = $scope.data.audios.length; i <l;i++)
+        {
+            console.log($scope.data.audios);
+           $scope.wave[i] = WaveSurfer.create({
+                      container: '#'+$scope.data.audios[i].name,
+                      height:200,
+                      barWidth:0.5,
+                      waveColor: '#e5e5ea',
+                      progressColor: "#FF6A6A",
+                      cursorWidth:2
+                  });
+          $scope.wave[i].load($scope.data.audios[i].audiolink);
+        }
+    }
   });
   $scope.trustSrc = function(src) {
     return $sce.trustAsResourceUrl(src);
-  }
-  console.log($scope.data);
-  if($scope.data.type === "video")
-  {
-      Video.initEvent($scope.data.nextChapter,$scope.data.nextSlide);
-  }
-  else if($scope.data.type === "audios")
-  {
-        var wavesurfer = WaveSurfer.create({
-        container: '#aurelien',
-        height:200,
-        barWidth:0.5,
-        waveColor: '#e5e5ea',
-        progressColor: "#FF6A6A",
-        cursorWidth:2
-    });
-
-    wavesurfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
-
-    wavesurfer.on('ready', function () {
-        wavesurfer.play();
-    });
-
   }
 
 });
